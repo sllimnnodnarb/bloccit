94f69fed1feb04b345b3d263d7a7a450a8c9868b
Brandon Mills <sllimnodnarb@gmail.com>
2017-04-04 20:58:09 -0500

    adding the file associated with assignment-17-seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,29 @@
 require 'random_data'
 
- 5.times do
-   Post.create!(
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
- posts = Post.all
+  5.times do
+    Post.create!(
+      title:  RandomData.random_sentence,
+      body:   RandomData.random_paragraph
+    )
+  end
 
- 10.times do
-   Comment.create!(
-     post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+  posts = Post.all
+
+  10.times do
+    Comment.create!(
+      post: posts.sample,
+      body: RandomData.random_paragraph
+    )
+  end
+
+  puts "#{Post.count}"
+  1.times do
+    Post.find_or_create_by(
+      title: "It is well",
+      body: "With my soul"
+    )
+  end
+  puts "#{Post.count}"
 
  puts "Seed finished"
  puts "#{Post.count} posts created"
