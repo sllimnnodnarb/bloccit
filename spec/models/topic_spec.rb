61b74396ec28e7d2c27b899a171aab69b1707fe2
Brandon Mills <sllimnodnarb@gmail.com>
2017-04-11 15:09:18 -0500

    adding all files associated with assignment-23-validations

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: name, description: description) }
 
   it { is_expected.to have_many(:posts) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+  it { is_expected.to validate_length_of(:name).is_at_least(5) }
+  it { is_expected.to validate_length_of(:description).is_at_least(15) }
 
   describe "attributes" do
     it "has name, description, and public attributes" do
@@ -16,7 +20,6 @@ RSpec.describe Topic, type: :model do
     it "is public by default" do
       expect(topic.public).to be(true)
     end
-
 
   end
 end
