0ff4c4a90d28362342cba6a0d83979b32f7e3870
Brandon Mills <sllimnodnarb@gmail.com>
2017-04-12 16:19:35 -0500

    adding all files associated wtih assignment-25-signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     resources :advertisements, except: [:index]
   end
   resources :users, only: [:new, :create]
-
+  post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
   root 'welcome#index'
 end
