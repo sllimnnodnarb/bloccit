ec0459fdf61e664c5efc475e8ef3690087956502
Brandon Mills <sllimnodnarb@gmail.com>
2017-04-12 19:35:16 -0500

    adding all files associated with assignment-26-sessions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :advertisements, except: [:index]
   end
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
   root 'welcome#index'
