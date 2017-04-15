03bddd3dc7ce6335e1e050289fe51cbf6538b2ea
Brandon Mills <sllimnodnarb@gmail.com>
2017-04-15 11:22:02 -0500

    adding all files associated with assignment-31-favorites

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,13 @@
 class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
+
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
   after_create :create_vote
+  after_create :create_favorite
 
   default_scope { order('rank DESC') }
   scope :ordered_by_title, -> { order('title ASC') }
@@ -38,6 +40,11 @@ class Post < ActiveRecord::Base
 
   def create_vote
     user.votes.create(value: 1, post: self)
+  end
+
+  def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 
 end
